3c22a31161c0ecf7ccf96239d4276d2a80c7292c
W1nter-IsHere <w1nter.ish3re@gmail.com>
2022-07-25 15:59:36 -0400

    Fixed painting buttons bug

--- Assets/Scripts/Games/Utils/Paint/PaintTools/PencilTool.cs
+++ Assets/Scripts/Games/Utils/Paint/PaintTools/PencilTool.cs
@@ -7,6 +7,7 @@ namespace Games.Utils.Paint.PaintTools
         public override void Initialize(DrawingSettings settings)
         {
             settings.SetMarkerWidth(1);
+            settings.SetMarkerColour(ColorPaletteExtension.GetColorFromCode(ColorPalette.Black));
         }
     }
 }
